b93599e8b101191fa23af5c007f1a3fc1d3a1eb7
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2025-05-21 10:49:06 +0200

    add local intrinsic dimensionality

--- hephaestus.py
+++ hephaestus.py
@@ -57,6 +57,30 @@ def relative_contrast(query, data, k, dist_fn):
     return avg / kth
 
 
+def local_intrinsic_dimensionality(query, data, k, dist_fn):
+    dists = dist_fn(query, data)
+    idxs = jnp.argpartition(dists, k)
+    dists = dists[idxs[:k]]
+    w = jnp.max(dists)
+    half_w = 0.5 * w
+
+    dists = dists[dists > 1e-5]
+
+    small = dists[dists < half_w]
+    large = dists[dists >= half_w]
+
+    s = np.log(small / w).sum() + np.log1p((large - w) / w).sum()
+    valid = small.size + large.size
+
+    return -valid / s
+    
+
+def query_expansion(query, data, k, dist_fn):
+    dists = dist_fn(query, data)
+    dists = jnp.sort(dists)
+    return ic(dists[2*k]) / ic(dists[k])
+
+
 def partition_by(candidates, fun):
     # first do an exponential search
     upper = 0
@@ -212,22 +236,25 @@ if __name__ == "__main__":
         data = jnp.array(hfp["train"][:])
         d = data.shape[1]
 
-    fig, axs = plt.subplots(1, 3)
+    fig, axs = plt.subplots(1, 3, figsize=(6,3))
 
     k = 10
     empirical = IVFEmpiricalHardness(Euclidean(), 0.9)
     empirical.fit(data)
 
+    distance = Euclidean()
+
     for target, ax in zip([4, 1.2, 1.05], axs):
         hg = HephaestusGradient(
-            Euclidean(), relative_contrast, learning_rate=1, max_iter=500, seed=1234
+            distance, relative_contrast, learning_rate=1, max_iter=500, seed=1234
         )
         hg.fit(data)
         q = hg.generate(k=k, rc_low=0.99 * target, rc_high=1.01 * target)
-        rc = relative_contrast(q, data, k, Euclidean())
-        emp = empirical.evaluate(q, k)
+        rc = relative_contrast(q, data, k, distance)
+        lid = local_intrinsic_dimensionality(q, data, k, distance)
+        emp = empirical.evaluate(q, k) * 100
         ax.imshow(q.reshape(28, 28))
-        ax.set_title(f"RC={rc:.2f}\nempirical={emp:.2f}")
+        ax.set_title(f"RC={rc:.2f}\nLID={lid:.2f}\nempirical={emp:.1f}%")
         ax.axis("off")
 
     plt.tight_layout()
